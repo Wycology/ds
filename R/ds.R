c68292b24aa37964ae2cccc0226ae8803826cd54
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-04-01 21:50:38 +0300

    Update ds.R

--- R/ds.R
+++ R/ds.R
@@ -13,7 +13,7 @@ ds <- function(x) {
   # Density plot
   plot(density(x))
   par(mfrow = c(1, 1))
-  # Some of the Numeric summary of the package
+  # Some of the numeric summary of the package
   data.frame(min = min(x),
              median = median(x),
              mean = mean(x),
